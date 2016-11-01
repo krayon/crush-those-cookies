70df82001a9b5f7b66ce098a1339d6c53146f3c7
wsdfhjxc <wsdfhjxc@protonmail.com>
2016-11-01 12:54:28 +0100

    Change the way window's close event listener is being handled
    This resolves #9.

--- content/js/windows.js
+++ content/js/windows.js
@@ -3,10 +3,45 @@ let EXPORTED_SYMBOLS = ["Windows"];
 Components.utils.import("resource://gre/modules/Services.jsm");
 
 let Windows = function(Tabs, Buttons, Crusher, Prefs) {
+    this.onCloseWindow = function(event) {
+        let domWindow = event.target;
+        if (Services) {
+            let windowsEnumerator = Services.wm.getEnumerator("navigator:browser");
+            let windowsCounter = 0;
+            
+            while (windowsEnumerator.hasMoreElements()) {
+                windowsEnumerator.getNext();
+                windowsCounter++;
+            }
+            
+            if (windowsCounter > 1 || Prefs.getValue("crushOnLastWindowClose")) {
+                let tabBrowser = domWindow.gBrowser;
+                
+                let domains = [];
+                
+                for (let tab of tabBrowser.tabs) {
+                    let browser = tab.linkedBrowser;
+                    let domain = browser.contentDocument.domain;
+                    
+                    domains.push(domain);
+                }
+                
+                let immediatelyForLastWindow = windowsCounter == 1;
+                
+                Crusher.prepare(domains, immediatelyForLastWindow);
+            }
+            
+            Tabs.clear(domWindow);
+            Buttons.clear(domWindow);
+        }
+    };
+    
     this.windowListener = {
         onOpenWindow: function(nsIObj) {
             let domWindow = nsIObj.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
                                   .getInterface(Components.interfaces.nsIDOMWindow);
+            
+            let that = this;
             
             domWindow.addEventListener("load", function() {
                 domWindow.removeEventListener("load", arguments.callee, false);
@@ -14,46 +49,15 @@ let Windows = function(Tabs, Buttons, Crusher, Prefs) {
                     if (domWindow.document.documentElement.getAttribute("windowtype") === "navigator:browser") {
                         Tabs.init(domWindow);
                         Buttons.init(domWindow);
+                        
+                        domWindow.addEventListener("close", that.onCloseWindow, true);
                     }
                 }, 0, domWindow);
             }, false);
-        },
-        
-        onCloseWindow: function(nsIObj) {
-            let domWindow = nsIObj.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
-                                  .getInterface(Components.interfaces.nsIDOMWindow);
-            
-            if (domWindow.document.documentElement.getAttribute("windowtype") === "navigator:browser") {
-                let windowsEnumerator = Services.wm.getEnumerator("navigator:browser");
-                let windowsCounter = 0;
-                
-                while (windowsEnumerator.hasMoreElements()) {
-                    windowsEnumerator.getNext();
-                    windowsCounter++;
-                }
-                
-                if (windowsCounter > 1 || Prefs.getValue("crushOnLastWindowClose")) {
-                    let tabBrowser = domWindow.gBrowser;
-                    
-                    let domains = [];
-                    
-                    for (let tab of tabBrowser.tabs) {
-                        let browser = tab.linkedBrowser;
-                        let domain = browser.contentDocument.domain;
-                        
-                        domains.push(domain);
-                    }
-                    
-                    let immediatelyForLastWindow = windowsCounter == 1;
-                    
-                    Crusher.prepare(domains, immediatelyForLastWindow);
-                }
-                
-                Tabs.clear(domWindow);
-                Buttons.clear(domWindow);
-            }
         }
     };
+    
+    this.windowListener.onOpenWindow = this.windowListener.onOpenWindow.bind(this);
 
     this.init = function(firstRun) {          
         let windowsEnumerator = Services.wm.getEnumerator("navigator:browser");
@@ -76,8 +80,11 @@ let Windows = function(Tabs, Buttons, Crusher, Prefs) {
         while (windowsEnumerator.hasMoreElements()) {
             let window = windowsEnumerator.getNext().QueryInterface(Components.interfaces.nsIDOMWindow);
             
-            Tabs.clear(window);
-            Buttons.clear(window);
+            if (window.document.documentElement.getAttribute("windowtype") === "navigator:browser") {
+                window.removeEventListener("close", this.onCloseWindow);
+                Tabs.clear(window);
+                Buttons.clear(window);
+            }
         }
     }
 };
