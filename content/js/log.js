b5dd74c8dca71ccd9db1d3b9bac287a9e83d4a3b
wsdfhjxc <wsdfhjxc@protonmail.com>
2016-11-07 19:52:56 +0100

    Use monospace date format in activity log

--- content/js/log.js
+++ content/js/log.js
@@ -7,9 +7,10 @@ let Log = function(Prefs) {
     this.log = function(crushedDomainsString) {
         if (Prefs.getValue("enableLogging") && crushedDomainsString) {
             let date = new Date();
-            let readableDate = date.getDate() + "." +
+            let readableDate = ("0" + date.getDate()).slice(-2) + "." +
                                ("0" + (date.getMonth() + 1)).slice(-2) + "." +
-                               date.getFullYear() + " " + date.getHours() + ":" +
+                               date.getFullYear() + " " +
+                               ("0" + date.getHours()).slice(-2) + ":" +
                                ("0" + date.getMinutes()).slice(-2) + ":" +
                                ("0" + date.getSeconds()).slice(-2);
             
